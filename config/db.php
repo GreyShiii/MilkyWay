2085261acc6f76d7f0a6f6213f2175e47f009b88
Grey <xandergrey@gmail.com>
2026-02-23 00:54:39 +0800

    fixed grammar in didyouknow

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php 
 $servername = "localhost";
 $username = "root";
-$password = "grey";
+$password = "";
 $database = "MilkyWay";
 
 $conn = new mysqli($servername, $username, $password, $database);
