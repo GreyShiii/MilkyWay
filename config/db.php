89d6a08eeca093e5d4ee7c8a7d9ba37e8c1a6508
Grey <xandergrey@gmail.com>
2026-02-22 12:34:43 +0800

    fixed css

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "grey";
 $database = "MilkyWay";
 
 $conn = new mysqli($servername, $username, $password, $database);
